d3e66416a8dad12a5c16f5e3930b300b01a51668
DemonHuntDW <45897191+DemonHuntDW@users.noreply.github.com>
2021-01-01 15:19:06 +0100

    Add files via upload

---
+++ Imagehelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cSharpIntroWinForms
+{
+    public class Imagehelper
+    {
+
+        static public byte[] FromImagetobyte(Image Slika)
+        {
+            MemoryStream Ms = new MemoryStream();
+            Slika.Save(Ms, ImageFormat.Jpeg);
+            return Ms.ToArray();
+        }
+        static public Image FrombytetoImage(byte[] slika)
+        {
+            MemoryStream Ms = new MemoryStream(slika);
+            return Image.FromStream(Ms);
+        }
+
+    }
+}
